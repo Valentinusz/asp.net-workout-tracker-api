9e6d5418aa8543c96139d033dc10c1f4fef6aaf4
Valentinusz <68827048+Valentinusz@users.noreply.github.com>
2025-01-17 23:01:24 +0100

    update

---
+++ WorkoutTrackerServer/Workouts/Service/IUserWorkoutService.cs
@@ -0,0 +1,8 @@
+﻿using WorkoutTrackerServer.Workouts.Dto;
+
+namespace WorkoutTrackerServer.Workouts.Service;
+
+public interface IUserWorkoutService
+{
+    Task<IEnumerable<WorkoutDto>> GetWorkoutsOfUser(long userId);
+}
---
+++ WorkoutTrackerServer/Workouts/Service/UserWorkoutService.cs
@@ -0,0 +1,14 @@
+﻿using WorkoutTrackerServer.Workouts.Dto;
+using WorkoutTrackerServer.Workouts.Repository;
+
+namespace WorkoutTrackerServer.Workouts.Service;
+
+public class UserWorkoutService : IUserWorkoutService
+{
+    private readonly IWorkoutRepository _workoutRepository;
+    
+    public async Task<IEnumerable<WorkoutDto>> GetWorkoutsOfUser(long userId)
+    {
+        return await _workoutRepository.FindByUserId();
+    }
+}
