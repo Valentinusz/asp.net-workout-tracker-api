af7b3e6fc0356b642ad90ece1c91ba23af9f2cf3
Valentinusz <68827048+Valentinusz@users.noreply.github.com>
2025-02-05 23:29:22 +0100

    update

---
+++ WorkoutTrackerServer/User/Service/UserService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WorkoutTrackerServer.User.Service;
+
+public class UserService
+{
+    
+    // TODO result pattern 
+    public Task<ProblemDetails> GetUser()
+    {
+        return null;
+    }
+}
